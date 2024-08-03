1942ee958a456e14b62c42778bb34bdd71e4e9b7
Lagedane <cyjae96@gmail.com>
2024-08-03 11:29:48 +0700

    Practice2_TextInput Component

--- components/Content.tsx
+++ components/Content.tsx
@@ -4,19 +4,20 @@ import { stylesPractice } from "../styles/styles";
 
 type ContentProps = {
   message: string;
-  fullname: string;
+  // fullname: string;
+  onButtonClick: () => void;
 };
-const Content = ({ message, fullname }: ContentProps): React.JSX.Element => {
-  const [displayFullname, setDisplayFullname] = useState("");
-  const handleButtonClick = () => {
-    setDisplayFullname(fullname);
-    Alert.alert(`'Hello', Input your fullname: ${fullname}`);
-  };
+const Content = ({ message, onButtonClick }: ContentProps): React.JSX.Element => {
+  // const [displayFullname, setDisplayFullname] = useState("");
+  // const handleButtonClick = () => {
+  //   setDisplayFullname(fullname);
+  //   Alert.alert(`'Hello', Input your fullname: ${fullname}`);
+  // };
   return (
     <View style={stylesPractice.content}>
       <Text style={stylesPractice.text}>{message}</Text>
-      <Text style={stylesPractice.text}>{displayFullname}</Text>
-      <Button title="Click Me" onPress={handleButtonClick} />
+      {/* <Text style={stylesPractice.text}>{displayFullname}</Text> */}
+      <Button title="Click Me" onPress={onButtonClick} />
     </View>
   );
 };
